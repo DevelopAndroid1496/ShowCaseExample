44e16b90eb5e6df419d480dbc24741e4e9053625
PEDRO IGNACIO DUARTE SANDIN <pedro.duartes@bancoazteca.com>
2024-10-31 21:26:06 -0600

    Avance demo coahMark

--- app/src/main/java/com/example/showcaseviewexample/MainActivity.kt
+++ app/src/main/java/com/example/showcaseviewexample/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.example.showcaseviewexample
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseView
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig
 
 class MainActivity : AppCompatActivity() {
 
-   /* private lateinit var showcaseView: ShowCaseView // Asegúrate de que sea el nombre correcto
+    /* private lateinit var showcaseView: ShowCaseView // Asegúrate de que sea el nombre correcto
     var target: View? = null*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,33 +26,43 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        initShowCase()
+
     }
 
-    private fun initShowCase(){
+    private fun initShowCase() {
 
-        /*ShowCaseView(this)
-        showcaseView = ShowCaseView(this).apply {
-            setTarget(target!!)
-            setTitle("Este es un ejemplo")
-            setDescription("Descripción del componente a mostrar")
+        val target = findViewById<View>(R.id.cv_five)
+        val element1 = findViewById<View>(R.id.iv_one)
+        val element2 = findViewById<View>(R.id.cv_two)
+        val element3 = findViewById<View>(R.id.iv_four)
+
+        val targets = arrayOf(element1,element2,element3)
+
+        MaterialShowcaseView.Builder(this)
+            .setMaskColour(resources.getColor(R.color.transparent)) //Color de fondo Aqui se mostraria el color transparente
+            .setTarget(target)
+            .setDismissTextColor(resources.getColor(R.color.black))
+            .setContentText("This is some amazing feature you should know about")
+            // optional but starting animations immediately in onCreate can make them choppy
+            //.singleUse("Material show view Case") // provide a unique ID used to ensure it is only shown once
+            .show()
+
+
+        val config = ShowcaseConfig().apply {
+            dismissTextColor = resources.getColor(R.color.black)
         }
 
-        // Agregar el ShowcaseView a la vista principal
-        //val mainShow = (findViewById<View>(android.R.id.content) as ViewGroup).addView(showcaseView)
 
-        // Manejar el toque para ocultar el ShowcaseView
-        showcaseView.setOnClickListener {
-            (findViewById<View>(android.R.id.content) as ViewGroup).removeView(showcaseView)
-        }*/
-    }
+        val sequence = MaterialShowcaseSequence(this).apply {
+            setConfig(config)
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        /*if (hasFocus){
-            val location = IntArray(2)
-            target?.getLocationOnScreen(location)
-            Log.d("TAG","COORDENADAS => \n X = ${target?.x}, Y = ${target?.y}")
-            initShowCase()
-        }*/
+            addSequenceItem(targets[0], "Operaciones \nLas operaciones que haces en sucursal tambien puedes hacerlas desde la App ", "Continuar")
+            addSequenceItem(targets[1], "Ayuda \nAqui encuentras las preguntas frecuentes", "Continuar")
+            addSequenceItem(targets[2], "Historias \nEncuentra contenido de interes para tus finanzas", "Aceptar")
+            start()
+
+        }
+
     }
 }
